59bf798a05c86821e79cb1aa10d5331860c12591
Divlex <alexey.roshchenko@simplbit.com>
2022-02-09 15:44:14 +0500

    First commit to kube-apply

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+# Extract the base64 encoded config data and write this to the KUBECONFIG
+echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
+export KUBECONFIG=/tmp/config
+export KUBE_NAMESPACE="$NAMESPACE"
+export KUBE_IMAGE="$IMAGE"
+export SELECTOR_NAME="$NAME"
+
+kubectl config current-context
+kubectl set image deployments/${SELECTOR_NAME} ${SELECTOR_NAME}=$DOCKER_REGISTRY_HOST/${KUBE_IMAGE} --namespace=${KUBE_NAMESPACE} --record
+#kubectl delete pods -l app=${SELECTOR_NAME} --namespace=${KUBE_NAMESPACE}
